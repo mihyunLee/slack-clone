c2f8c27b7cfd552d02558afba8af09c49e3d8159
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-13 16:44:56 +0900

    ✨feat: loadable-component를 사용하여 코드 스플리팅 적용

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,7 +1,9 @@
-import Login from '@pages/Login';
-import SignUp from '@pages/SignUp';
 import React from 'react';
+import loadable from '@loadable/component';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
