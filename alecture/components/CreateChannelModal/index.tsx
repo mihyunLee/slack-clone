614aa8c641bc151165328a78b387e29809d036d7
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-26 21:11:33 +0900

    ✨feat: 채널 추가 기능 구현

--- alecture/components/CreateChannelModal/index.tsx
+++ alecture/components/CreateChannelModal/index.tsx
@@ -1,17 +1,54 @@
 import Modal from '@components/Modal';
 import useInput from '@hooks/useInput';
 import { Button, Input, Label } from '@pages/SignUp/styles';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
 import React, { useCallback } from 'react';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import useSWR from 'swr';
 
 interface Props {
   show: boolean;
   onCloseModal: () => void;
+  setShowCreateChannelModal: (flag: boolean) => void;
 }
 
-const CreateChannelModal: React.FC<Props> = ({ show, onCloseModal }) => {
-  const [newChannel, onChangeNewChannel] = useInput('');
+const CreateChannelModal: React.FC<Props> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
 
-  const onCreateChannel = useCallback(() => {}, []);
+  let { workspace } = useParams<{ workspace?: string }>();
+
+  const { data: userData, error, mutate } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
+  const { mutate: mutateChannel } = useSWR<IChannel[]>(
+    userData ? `http://localhost:3095/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
+  const onCreateChannel = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      axios
+        .post(
+          `http://localhost:3095/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          { withCredentials: true },
+        )
+        .then(() => {
+          setShowCreateChannelModal(false);
+          mutateChannel();
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel, workspace],
+  );
 
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
