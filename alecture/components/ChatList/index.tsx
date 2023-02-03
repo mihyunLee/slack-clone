1c501d799d075f7c7e9154f89cc0c9ce9375962e
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-02-03 13:30:53 +0900

    ✨feat: 스크롤바 라이브러리 적용

--- alecture/components/ChatList/index.tsx
+++ alecture/components/ChatList/index.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import Chat from '@components/Chat';
 import { IDM } from '@typings/db';
 import { ChatZone } from './styles';
+import Scrollbars from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: React.FC<Props> = ({ chatData }) => {
+  const scrollRef = useRef(null);
+
+  const onScroll = useCallback(() => {
+    // TODO: 스크롤을 맨 위로 올렸을 때, 이전 채팅 보여주기
+  }, []);
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
