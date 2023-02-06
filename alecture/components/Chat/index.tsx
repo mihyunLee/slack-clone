1ce89c34ffa310bf40ae942dc2b30ae4af628b87
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-02-06 09:59:38 +0900

    ✨feat: regexifyString 설치 및 멘션에 적용

--- alecture/components/Chat/index.tsx
+++ alecture/components/Chat/index.tsx
@@ -3,13 +3,33 @@ import { IDM } from '@typings/db';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: React.FC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string }>();
+
   const user = data.Sender;
+
+  const result = regexifyString({
+    input: data.content,
+    pattern: /@\[(.+?)\]\((\d+?)\)|\n]/g,
+    decorator(match, index) {
+      const arr: string[] | null = match.match(/@\[(.+?)\]\((\d+?)\)/)!;
+      if (arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+            @{arr[1]}
+          </Link>
+        );
+      }
+      return <br key={index} />;
+    },
+  });
 
   return (
     <ChatWrapper>
@@ -21,7 +41,7 @@ const Chat: React.FC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
